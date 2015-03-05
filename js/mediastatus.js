feb199abb2f47f1b76eb18ce764e8ed83ab22a4f
Peng Ying <pying@google.com>
2015-03-04 18:17:44 -0800

    finishing video syncing

--- js/mediastatus.js
+++ js/mediastatus.js
@@ -2,12 +2,26 @@ var cast = window.cast || {};
 
 (function () {
   'use strict';
+  var loadCoreSignal = function () {
+    var coreSignalImport = document.createElement('link');
+    coreSignalImport.setAttribute('rel', 'import');
+    coreSignalImport.setAttribute('href', '../bower_components/core-signals/core-signals.html');
+    document.body.appendChild(coreSignalImport);
+  };
+
+  if (document.readyState === "complete") {
+    loadCoreSignal();
+  } else {
+    document.addEventListener("DOMContentLoaded", function () {
+      loadCoreSignal();
+    });
+  }
+
 
   MediaStatus.STATE = {
-    'PAUSE': 0,
-    'PLAY': 1,
-    'STOP': 2,
-    'SEEK': 3
+    'PAUSE': 0, //has started playing but is currently paused
+    'PLAY': 1, //currently playing
+    'STOP': 2 //has never started playing
   };
 
   /**
@@ -17,7 +31,7 @@ var cast = window.cast || {};
    * @param mediaData
    * @constructor
    */
-  function MediaStatus (mediaData) {
+  function MediaStatus(mediaData) {
     this.mediaData = mediaData;
     this.media = null;
     this.session = {};
@@ -25,7 +39,6 @@ var cast = window.cast || {};
     this.timeRemainingString = "0:00:00";
     this.durationS = 50000;
     this.currentTimeS;
-    this.seekTimeS;
     this.volume = 50;
     this.isCasting = false;
     this.castMediaData = null;
@@ -33,44 +46,55 @@ var cast = window.cast || {};
   }
 
   MediaStatus.prototype = {
-    play: function() {
+    play: function () {
       this.state = MediaStatus.STATE.PLAY;
     },
     pause: function () {
       this.state = MediaStatus.STATE.PAUSE;
     },
-    seek: function(time) {
-      this.previousState = this.state;
-      this.state = MediaStatus.STATE.SEEK;
-      this.seekTimeS = time;
-      if(this.durationS != 0){
+    seek: function (time, sender) {
+      //Fire a core-signal event
+      var seekEvent = new CustomEvent('core-signal',
+          {
+            'detail': {
+              'name': 'seek',
+              'data': {
+                'currentTime': time,
+                'sender': sender
+              }
+            }
+          });
+      document.dispatchEvent(seekEvent);
+
+      if (this.durationS != 0) {
         this.timeRemainingString = this.secondsToHHMMSS(this.durationS - time);
       }
     },
-    toPreviousState: function() {
-      this.state = this.previousState;
-    },
-    setDuration: function(durationS) {
+    setDuration: function (durationS) {
       this.durationS = durationS;
       this.timeRemainingString = this.secondsToHHMMSS(durationS);
     },
-    setCurrentTime:function(timeS) {
+    setCurrentTime: function (timeS) {
       this.currentTimeS = timeS;
       this.timeRemainingString = this.secondsToHHMMSS(this.durationS - timeS);
     },
-    getCurrentPercentageComplete: function() {
-      return this.currentTimeS/this.durationS;
+    getCurrentPercentageComplete: function () {
+      return this.currentTimeS / this.durationS;
     },
-    floatToTime: function(floatVal) {
+    floatToTime: function (floatVal) {
       return floatVal * this.durationS;
     },
     secondsToHHMMSS: function (seconds) {
       var hours = Math.floor(seconds / 3600);
-      var minutes = Math.floor((seconds % 3600)/60);
+      var minutes = Math.floor((seconds % 3600) / 60);
       var seconds = Math.floor(seconds % 60);
 
-      if (minutes < 10) {minutes = "0" + minutes;}
-      if (seconds < 10) {seconds = "0" + seconds;}
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
 
       return hours + ':' + minutes + ':' + seconds;
     }
